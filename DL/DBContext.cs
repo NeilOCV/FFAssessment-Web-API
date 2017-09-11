6372e1e4ad2ac6ddfdd14244e9ea6a17820024b7
NeilOCV <31700601+NeilOCV@users.noreply.github.com>
2017-09-11 07:14:45 +0200

    201707110714
    Changes to the precision of the longetude and latitude.

--- DL/DBContext.cs
+++ DL/DBContext.cs
@@ -6,7 +6,27 @@ namespace DL
 
     public class DBContext : DbContext
     {
+        
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Contact> Contacts { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>().Property(c => c.name).HasMaxLength(50);
+            modelBuilder.Entity<Customer>().Property(c => c.name).IsRequired();
+            modelBuilder.Entity<Customer>().Property(c => c.latitude).HasPrecision(12, 9);
+            modelBuilder.Entity<Customer>().Property(c => c.latitude).IsRequired();
+            modelBuilder.Entity<Customer>().Property(c => c.longetude).HasPrecision(12, 9);
+            modelBuilder.Entity<Customer>().Property(c => c.longetude).IsRequired();
+
+            modelBuilder.Entity<Contact>().Property(c => c.name).HasMaxLength(50);
+            modelBuilder.Entity<Contact>().Property(c => c.name).IsRequired();
+            modelBuilder.Entity<Contact>().Property(c => c.number).HasMaxLength(50);
+            modelBuilder.Entity<Contact>().Property(c => c.email).HasMaxLength(50);
+            modelBuilder.Entity<Contact>().Property(c => c.email).IsRequired();
+            modelBuilder.Entity<Contact>().Property(c => c.customer_id).IsRequired();
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
